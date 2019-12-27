3fbdd292d74fd86b9f17dfe0458cad8dafd0d2fa
Alberto Díaz Alonso <watershark9@gmail.com>
2019-12-26 21:05:46 -0400

    Mod Fundamentals

---
+++ src/main/java/com/watershark9/dsjarmory/proxy/CommonProxy.java
@@ -0,0 +1,5 @@
+package com.watershark9.dsjarmory.proxy;
+
+public class CommonProxy {
+
+}
---
+++ src/main/java/com/watershark9/dsjarmory/util/Reference.java
@@ -0,0 +1,11 @@
+package com.watershark9.dsjarmory.util;
+
+public class Reference {
+
+	public static final String MOD_ID = "da";
+	public static final String NAME = "DSJ Armory";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "com.watershark9.dsjarmory.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "com.watershark9.dsjarmory.proxy.CommonProxy";
+}
